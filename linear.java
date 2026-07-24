697e7e9c3469b30dee26b18e0c5f151b1787bf4e
gobikaram16 <ramakrishnangobikaramakrishnan@gmail.com>
2026-07-24 18:16:54 +0530

    Add files via upload

---
+++ linear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class linear {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        int x = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] == x) {
+                System.out.println("Found");
+                return;
+            }
+        }
+
+        System.out.println("Not Found");
+    }
+}
